90ef12e376f433ee397524f9fed8e9885309d9e0
Sabin <sabingt0@gmail.com>
2022-11-07 09:23:34 +0545

    changed the docker run option to imagemagick docker

--- test.sh
+++ test.sh
@@ -11,5 +11,5 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
   --user $(id -u):$(id -g) \
-  gautamsabin/docker-robot-framework:v1.0
+  sabingautam07/robotframework-imagemagick-docker
  
