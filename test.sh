f65eedc0f597745fa285a738a1e24b1369496681
Sabin <sabingt0@gmail.com>
2022-11-15 10:48:29 +0545

    chnaged the docker image

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
-  -e BROWSER=$BROWSER \
+    
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
   --user $(id -u):$(id -g) \
-  sabingautam07/robotframework-imagemagick-docker:latest
+  standan/robot:latest
  
